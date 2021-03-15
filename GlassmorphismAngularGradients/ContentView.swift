f3bb3b3941f1e7834a56676bc52b0b21c836e4bb
Terry <terrykuo501@gmail.com>
2021-03-15 14:38:34 +0800

    Initial Commit

---
+++ GlassmorphismAngularGradients/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlassmorphismAngularGradients
+//
+//  Created by Terry Kuo on 2021/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GlassmorphismAngularGradientsTests/GlassmorphismAngularGradientsTests.swift
@@ -0,0 +1,33 @@
+//
+//  GlassmorphismAngularGradientsTests.swift
+//  GlassmorphismAngularGradientsTests
+//
+//  Created by Terry Kuo on 2021/3/15.
+//
+
+import XCTest
+@testable import GlassmorphismAngularGradients
+
+class GlassmorphismAngularGradientsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
